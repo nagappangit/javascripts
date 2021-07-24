01fd3355b6ba03bf8921e70e422da0ae37bc6e71
nagappangit <nagarajeshbe@gmail.com>
2021-07-24 13:31:23 +0530

    commit

--- stringslice.js
+++ stringslice.js
@@ -1,2 +1,5 @@
 let str = "Welcome Mr.Nagappan";
 console.log(str.slice(8,15));
+console.log(str.slice(-15,-8));
+console.log(str.slice(8));//If you omit the second parameter, the method will slice out the rest of the string Characters
+console.log(str.slice(-15)); // it counting from the left
