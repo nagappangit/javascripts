fa8bf27ddcbca71ee0f56f2b55bbc3f4be9673a2
nagappangit <nagarajeshbe@gmail.com>
2021-08-03 15:37:04 +0530

    push

--- array.js
+++ array.js
@@ -1,7 +1,26 @@
-let Cars = ["Sedan", "Etos", "SUV"];
-Cars[0] = "Compass";
+var Cars = ["Sedan", "Etos", "SUV"];
+//Cars[0] = "Compass";
 console.log(Cars);
 console.log(Cars[1]);
 const person = {firstName:"Nagappan", lastName:"Kumaresan", age:36};
 console.log(person);
 console.log(person.firstName);
+console.log(Cars.length);
+console.log(Cars[0]);
+console.log(Cars[Cars.length - 1]);
+function CarDet()
+{
+    let CarLen = Cars.length;
+    for(let i = 0; i < CarLen; i++)
+    {
+        console.log(Cars[i]);      
+    }
+}
+console.log(CarDet());
+
+console.log(Cars.forEach(myFunction));
+
+function myFunction(value)
+{
+    console.log(value);
+}
